14fd189b19ba900f23fa0927b0d465b134bdfa2c
Yury Adamov <yury.adamov@promwad.com>
2019-11-29 13:38:41 +0300

    Build only raspbian

--- scripts/ci_build.sh
+++ scripts/ci_build.sh
@@ -21,22 +21,25 @@ BUILD_DIR=./build
 BB_ARGS="-v"
 
 TARGETS_SET="\
-            multiconfig:qemuarm-stretch:isar-image-base \
-            multiconfig:qemuarm-buster:isar-image-base \
-            multiconfig:qemuarm-bullseye:isar-image-base \
-            multiconfig:qemuarm64-stretch:isar-image-base \
-            multiconfig:qemui386-stretch:isar-image-base \
-            multiconfig:qemui386-buster:isar-image-base \
-            multiconfig:qemui386-bullseye:isar-image-base \
-            multiconfig:qemuamd64-stretch:isar-image-base \
-            multiconfig:qemuamd64-buster:isar-image-base \
-            multiconfig:qemuamd64-buster-tgz:isar-image-base \
-            multiconfig:qemuamd64-bullseye:isar-image-base \
-            multiconfig:qemumipsel-stretch:isar-image-base \
-            multiconfig:qemumipsel-buster:isar-image-base \
-            multiconfig:qemumipsel-bullseye:isar-image-base \
-            multiconfig:nand-ubi-demo-buster:isar-image-ubi \
-            multiconfig:rpi-stretch:isar-image-base"
+    multiconfig:rpi-stretch:isar-image-base"
+
+#TARGETS_SET="\
+#            multiconfig:qemuarm-stretch:isar-image-base \
+#            multiconfig:qemuarm-buster:isar-image-base \
+#            multiconfig:qemuarm-bullseye:isar-image-base \
+#            multiconfig:qemuarm64-stretch:isar-image-base \
+#            multiconfig:qemui386-stretch:isar-image-base \
+#            multiconfig:qemui386-buster:isar-image-base \
+#            multiconfig:qemui386-bullseye:isar-image-base \
+#            multiconfig:qemuamd64-stretch:isar-image-base \
+#            multiconfig:qemuamd64-buster:isar-image-base \
+#            multiconfig:qemuamd64-buster-tgz:isar-image-base \
+#            multiconfig:qemuamd64-bullseye:isar-image-base \
+#            multiconfig:qemumipsel-stretch:isar-image-base \
+#            multiconfig:qemumipsel-buster:isar-image-base \
+#            multiconfig:qemumipsel-bullseye:isar-image-base \
+#            multiconfig:nand-ubi-demo-buster:isar-image-ubi \
+#            multiconfig:rpi-stretch:isar-image-base"
           # qemu-user-static of <= buster too old to build that
           # multiconfig:qemuarm64-buster:isar-image-base
           # multiconfig:qemuarm64-bullseye:isar-image-base
