a6b9e8a870e8d15d2c84c88f55ce6e907207610d
Yury Adamov <yury.adamov@promwad.com>
2019-12-11 12:05:21 +0300

    Checked out old version of ci_build.sh

--- scripts/ci_build.sh
+++ scripts/ci_build.sh
@@ -21,40 +21,35 @@ BUILD_DIR=./build
 BB_ARGS="-v"
 
 TARGETS_SET="\
-    multiconfig:qemuamd64-stretch:isar-image-base"
-
-#TARGETS_SET="\
-#            multiconfig:qemuarm-stretch:isar-image-base \
-#            multiconfig:qemuarm-buster:isar-image-base \
-#            multiconfig:qemuarm-bullseye:isar-image-base \
-#            multiconfig:qemuarm64-stretch:isar-image-base \
-#            multiconfig:qemui386-stretch:isar-image-base \
-#            multiconfig:qemui386-buster:isar-image-base \
-#            multiconfig:qemui386-bullseye:isar-image-base \
-#            multiconfig:qemuamd64-stretch:isar-image-base \
-#            multiconfig:qemuamd64-buster:isar-image-base \
-#            multiconfig:qemuamd64-buster-tgz:isar-image-base \
-#            multiconfig:qemuamd64-bullseye:isar-image-base \
-#            multiconfig:qemumipsel-stretch:isar-image-base \
-#            multiconfig:qemumipsel-buster:isar-image-base \
-#            multiconfig:qemumipsel-bullseye:isar-image-base \
-#            multiconfig:nand-ubi-demo-buster:isar-image-ubi \
-#            multiconfig:rpi-stretch:isar-image-base"
+            multiconfig:qemuarm-stretch:isar-image-base \
+            multiconfig:qemuarm-buster:isar-image-base \
+            multiconfig:qemuarm-bullseye:isar-image-base \
+            multiconfig:qemuarm64-stretch:isar-image-base \
+            multiconfig:qemui386-stretch:isar-image-base \
+            multiconfig:qemui386-buster:isar-image-base \
+            multiconfig:qemui386-bullseye:isar-image-base \
+            multiconfig:qemuamd64-stretch:isar-image-base \
+            multiconfig:qemuamd64-buster:isar-image-base \
+            multiconfig:qemuamd64-buster-tgz:isar-image-base \
+            multiconfig:qemuamd64-bullseye:isar-image-base \
+            multiconfig:qemumipsel-stretch:isar-image-base \
+            multiconfig:qemumipsel-buster:isar-image-base \
+            multiconfig:qemumipsel-bullseye:isar-image-base \
+            multiconfig:nand-ubi-demo-buster:isar-image-ubi \
+            multiconfig:rpi-stretch:isar-image-base"
           # qemu-user-static of <= buster too old to build that
           # multiconfig:qemuarm64-buster:isar-image-base
           # multiconfig:qemuarm64-bullseye:isar-image-base
 
 CROSS_TARGETS_SET="\
-    multiconfig:qemuamd64-stretch:isar-image-base"
-#CROSS_TARGETS_SET="\
-#                  multiconfig:qemuarm-stretch:isar-image-base \
-#                  multiconfig:qemuarm-buster:isar-image-base \
-#                  multiconfig:qemuarm-bullseye:isar-image-base \
-#                  multiconfig:qemuarm64-stretch:isar-image-base \
-#                  multiconfig:qemuamd64-stretch:isar-image-base \
-#                  multiconfig:de0-nano-soc-stretch:isar-image-base \
-#                  multiconfig:rpi-stretch:isar-image-base"
-#
+                  multiconfig:qemuarm-stretch:isar-image-base \
+                  multiconfig:qemuarm-buster:isar-image-base \
+                  multiconfig:qemuarm-bullseye:isar-image-base \
+                  multiconfig:qemuarm64-stretch:isar-image-base \
+                  multiconfig:qemuamd64-stretch:isar-image-base \
+                  multiconfig:de0-nano-soc-stretch:isar-image-base \
+                  multiconfig:rpi-stretch:isar-image-base"
+
 REPRO_TARGETS_SET="\
             multiconfig:qemuarm-stretch:isar-image-base \
             multiconfig:qemuarm64-stretch:isar-image-base \
@@ -113,10 +108,10 @@ do
         BB_ARGS=""
         ;;
     -r|--repro)
-        #REPRO_BUILD="1"
-        #case "$2" in
-        #-s|--sign) SIGN_REPO='1'; shift ;;
-        #esac
+        REPRO_BUILD="1"
+        case "$2" in
+        -s|--sign) SIGN_REPO='1'; shift ;;
+        esac
         ;;
     *)
         echo "error: invalid parameter '$key', please try '--help' to get list of supported parameters"
